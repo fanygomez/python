fef8e16523ad8c84855ecff89b0af49a12a32e72
Fany Gómez <fanygomez1894@gmail.com>
2021-04-28 19:52:26 -0600

    Submodulos

---
+++ calculos/basicos/operaciones_basicas.py
@@ -0,0 +1,10 @@
+def sumar(op1,op2):
+    print("Resultado es :", op1 + op2)
+
+def restar(op1,op2):
+    print("Resultado es :", op1 - op2)
+
+def multiplicar(op1,op2):
+    print("Resultado es :", op1 * op2)
+def dividir(op1,op2):
+    print("Resultado es :", op1 / op2)
---
+++ calculos/calculos_generales.py
@@ -0,0 +1,5 @@
+def potencia(base,exponente):
+    print("El resultado es ",base**exponente)
+
+def redondear(numero):
+    print("El resultado es:",round(numero))
