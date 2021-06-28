76079b0e46de7a497c508830ff1ec39132173bd7
Fany Gómez <fanygomez1894@gmail.com>
2021-06-27 21:29:04 -0600

    Python: Decoradores -Para que la  Funcion decoradora reciba n parametros, se agrego *args.

--- decoradores/ejemplo_01.py
+++ decoradores/ejemplo_01.py
@@ -1,20 +1,22 @@
 #Agregar funcionalidad  imprimit antes y despues de mostrar el resultado
+# def funcion_decararadoraA(funcion_parametroB):
+#arg numero indefinod de parameteos
 def funcion_decararadoraA(funcion_parametroB):
-    def funcion_interiorC():
+    def funcion_interiorC(*args):
         #Acciones decoradoras
         print("Se realizara el calculo: ")
-        funcion_parametroB()
+        funcion_parametroB(*args)
         print("Hemos terminado el cálculo")
     return funcion_interiorC
 #@Nombre de funcion decoradora
 
 @funcion_decararadoraA
-def suma():
-    print(15+20)
+def suma(num1,num2):
+    print(num1+num2)
 @funcion_decararadoraA
-def resta():
-    print(30-10)
+def resta(num1,num2):
+    print(num1-num2)
 
 print("Test...")
-suma()
-resta()
+suma(20,20)
+resta(30,10)
