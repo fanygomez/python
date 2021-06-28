0d78db0fdbdb6eb4a7ba5b30e72739090ff41a66
Fany Gómez <fanygomez1894@gmail.com>
2021-06-27 21:34:12 -0600

    Python: Decoradores -Para que la  Funcion decoradora reciba argumentos clave -> valor, se uso  **kwargs

--- decoradores/ejemplo_01.py
+++ decoradores/ejemplo_01.py
@@ -2,10 +2,12 @@
 # def funcion_decararadoraA(funcion_parametroB):
 #arg numero indefinod de parameteos
 def funcion_decararadoraA(funcion_parametroB):
-    def funcion_interiorC(*args):
+    # def funcion_interiorC(*args):
+    #** kwargs: paramtros clave valor
+    def funcion_interiorC(*args, **kwargs):
         #Acciones decoradoras
         print("Se realizara el calculo: ")
-        funcion_parametroB(*args)
+        funcion_parametroB(*args, **kwargs)
         print("Hemos terminado el cálculo")
     return funcion_interiorC
 #@Nombre de funcion decoradora
@@ -16,7 +18,11 @@ def suma(num1,num2):
 @funcion_decararadoraA
 def resta(num1,num2):
     print(num1-num2)
-
+@funcion_decararadoraA
+def potencia (base,exponente):
+    print(pow(base,exponente))
 print("Test...")
 suma(20,20)
 resta(30,10)
+#argumetnos con clave valor
+potencia(base=5,exponente=3)
